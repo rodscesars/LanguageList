5f6fadc1d51048635ea8468acb5b0db9aa71d9d0
Rodrigo Mendes <rodrigocesarsm@gmail.com>
2023-06-19 07:06:53 -0300

    add theme color and PaperProvider

--- App.js
+++ App.js
@@ -2,15 +2,19 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { store } from './src/features/store';
+import { PaperProvider } from 'react-native-paper';
 import Home from './src/app/screens/Home';
+import { theme } from './themes/theme';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Home />
-        <StatusBar style="auto" />
-      </View>
+      <PaperProvider theme={theme}>
+        <View style={styles.container}>
+          <StatusBar style="auto" backgroundColor="transparent" translucent />
+          <Home />
+        </View>
+      </PaperProvider>
     </Provider>
   );
 }
